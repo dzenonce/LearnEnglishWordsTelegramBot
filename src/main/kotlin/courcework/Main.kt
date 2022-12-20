8908981354b508d0b58bcf5eb2970d07ea7aff1a
dzenonce <markin-dmitrij@yandex.ru>
2022-12-20 23:11:53 +0300

    KS-CW-2 Создал dictionary, добавил счетчик выученных слов

---
+++ src/main/kotlin/courcework/Main.kt
@@ -0,0 +1,50 @@
+package courcework
+
+import java.io.File
+
+data class Word(
+    val original: String,
+    val translate: String,
+    val correctAnswersCount: Int = 0,
+) {
+
+    var countLearnedWord: Int = 0
+
+}
+
+fun main() {
+
+    val dictionary: MutableList<Word> = mutableListOf()
+
+    val wordsTxt = File("C:\\Users\\User\\IdeaProjects\\LearnEnglishWordsTelegramBot\\LearnEnglishWordsTelegramBot\\src\\main\\resources\\words.txt")
+    val countLearnedWordTxt = File("C:\\Users\\User\\IdeaProjects\\LearnEnglishWordsTelegramBot\\LearnEnglishWordsTelegramBot\\src\\main\\resources\\countLearnedWord.txt")
+
+    var counterLearnedWord = 0
+
+    wordsTxt.forEachLine { text ->
+        val line = text.split("|")
+
+        val word = Word(
+            original = line[0],
+            translate = line[1],
+            correctAnswersCount = line[2].toIntOrNull() ?: 0,
+        )
+
+        dictionary.add(word)
+
+        when (line[2].toIntOrNull() ?: 0) {
+            5 -> {
+                counterLearnedWord++
+
+                word.countLearnedWord = counterLearnedWord
+                countLearnedWordTxt.writeText(word.countLearnedWord.toString())
+            }
+        }
+
+    }
+
+    dictionary.forEach { println(it) }
+
+    println("Количество выученных слов: ${countLearnedWordTxt.readLines()}")
+
+}
