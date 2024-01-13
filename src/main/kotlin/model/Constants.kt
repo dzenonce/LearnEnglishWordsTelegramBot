9dc22958fa8fd412ed8172164c38dfacf3bf4c8f
dzenonce <markin-dmitrij@yandex.ru>
2024-01-13 21:10:01 +0300

    KS-CW-9 Сделал отправку результата, рабочее меню

---
+++ src/main/kotlin/model/Constants.kt
@@ -0,0 +1,12 @@
+package model
+
+const val STATISTICS_CLICKED = "statistics_clicked"
+const val LEARN_WORDS_CLICKED = "learn_words_clicked"
+const val CALLBACK_DATA_ANSWER_PREFIX = "answer_"
+const val UPDATE_ID = "updateId"
+const val CHAT_ID = "chatId"
+const val MESSAGE_TEXT = "messageText"
+const val CALLBACK_DATA = "callbackData"
+const val ALL_WORDS_LEARNED = "Вы выучили все слова в базе"
+const val CORRECT_ANSWER = "Правильно"
+const val WRONG_ANSWER = "Не правильно"
