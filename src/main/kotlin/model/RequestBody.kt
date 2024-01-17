885fc367406f38b154c71430ffe52300fa7f2074
dzenonce <markin-dmitrij@yandex.ru>
2024-01-17 16:47:47 +0300

    KS-CW-10 Изменил ориентацию кнопок выбора слов

--- src/main/kotlin/model/RequestBody.kt
+++ src/main/kotlin/model/RequestBody.kt
@@ -5,27 +5,27 @@ import model.serialization.ReplyMarkup
 import model.serialization.SendMessageRequest
 
 fun getMainMenu(chatId: Long) =
-        SendMessageRequest(
-            chatId = chatId,
-            text = TEXT_MAIN_MENU,
-            replyMarkup = ReplyMarkup(
-                inlineKeyboard =
+    SendMessageRequest(
+        chatId = chatId,
+        text = TEXT_MAIN_MENU,
+        replyMarkup = ReplyMarkup(
+            inlineKeyboard =
+            listOf(
                 listOf(
-                    listOf(
-                        InlineKeyboard(
-                            text = TEXT_LEARN_WORDS,
-                            callbackData = CALLBACK_LEARN_WORDS_CLICKED,
-                        )
-                    ),
-                    listOf(
-                        InlineKeyboard(
-                            text = TEXT_STATISTICS_MENU,
-                            callbackData = CALLBACK_MENU_STATISTICS_CLICKED,
-                        )
-                    ),
-                )
+                    InlineKeyboard(
+                        text = TEXT_LEARN_WORDS,
+                        callbackData = CALLBACK_LEARN_WORDS_CLICKED,
+                    )
+                ),
+                listOf(
+                    InlineKeyboard(
+                        text = TEXT_STATISTICS_MENU,
+                        callbackData = CALLBACK_MENU_STATISTICS_CLICKED,
+                    )
+                ),
             )
         )
+    )
 
 fun getStatisticsMenu(chatId: Long) =
     SendMessageRequest(
@@ -56,27 +56,31 @@ fun getStatisticsMenu(chatId: Long) =
     )
 
 fun getLearnWordsMenuBody(chatId: Long, question: Question): SendMessageRequest {
+
     val wordsList =
         question.fourUnlearnedWords.mapIndexed { index, word ->
-            InlineKeyboard(
-                text = word.original,
-                callbackData = "$CALLBACK_ANSWER_PREFIX$index"
+            listOf(
+                InlineKeyboard(
+                    text = word.original,
+                    callbackData = "$CALLBACK_ANSWER_PREFIX$index"
+                )
             )
-        }
+        }.toMutableList()
+
+    val exitMainMenuButton =
+        listOf(
+            InlineKeyboard(
+                text = TEXT_MAIN_MENU,
+                callbackData = CALLBACK_EXIT_MAIN_MENU_CLICKED,
+            )
+        )
+    wordsList.add(exitMainMenuButton)
 
     return SendMessageRequest(
         chatId = chatId,
         text = question.correctWord.translate,
         replyMarkup = ReplyMarkup(
-            listOf(
-                wordsList,
-                listOf(
-                    InlineKeyboard(
-                        text = TEXT_MAIN_MENU,
-                        callbackData = CALLBACK_EXIT_MAIN_MENU_CLICKED,
-                    )
-                )
-            )
+            wordsList,
         )
     )
 }
