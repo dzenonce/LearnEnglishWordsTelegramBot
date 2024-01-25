f4152e04a4f52beb05d85257a21234d73f38a4ca
dzenonce <markin-dmitrij@yandex.ru>
2024-01-25 14:53:02 +0300

    KS-CW-11 Изменил место обработки ошибок

--- src/main/kotlin/Telegram.kt
+++ src/main/kotlin/Telegram.kt
@@ -25,17 +25,11 @@ fun main(args: Array<String>) {
     var lastUpdateId = 0L
     while (true) {
         Thread.sleep(PAUSE_TELEGRAM_GET_UPDATE)
-        val response: Response =
-            try {
-                telegram.getUpdates(lastUpdateId)
-            } catch (e: Error) {
-                println("Get Updates with error: ${e.message}")
-                continue
-            }
+        val response: Response? = telegram.getUpdates(lastUpdateId)
 
-        if (response.result.isEmpty()) continue
-        val sortedUpdate = response.result.sortedBy { it.updateId }
-        sortedUpdate.forEach {
+        if (response?.result?.isEmpty() == true) continue
+        val sortedUpdate = response?.result?.sortedBy { it.updateId }
+        sortedUpdate?.forEach {
             handleUpdate(
                 update = it,
                 json = json,
@@ -43,7 +37,7 @@ fun main(args: Array<String>) {
                 botToken = botToken,
             )
         }
-        lastUpdateId = sortedUpdate.last().updateId + 1
+        lastUpdateId = sortedUpdate?.last()?.updateId?.plus(1) ?: 0L
     }
 }
 
@@ -75,35 +69,20 @@ fun handleUpdate(
     }
 
     if (document != null) {
-        try {
-            getUserWordsFileAndSave(
-                chatId = chatId,
-                document = document,
-                telegram = telegram,
-            )
-        } catch (e: Error) {
-            println("Ошибка получения пользовательского файла ${e.message}")
-        }
+        getUserWordsFileAndSave(
+            chatId = chatId,
+            document = document,
+            telegram = telegram,
+        )
         trainer.reloadDictionary()
-
-        try {
-            telegram.deleteMessage(
-                chatId = chatId,
-                messageId = messageId
-            )
-        } catch (e: Error) {
-            println("Ошибка удаления сообщения ползователя: ${e.message}")
-        }
+        telegram.deleteMessage(
+            chatId = chatId,
+            messageId = messageId
+        )
     }
 
     val mainMenuBody = getBodyMainMenu(chatId)
-    if (message == "/start")
-        try {
-            telegram.sendMenu(mainMenuBody)
-        } catch (e: Error) {
-            println("Ошибка при отправке главного меню: ${e.message}")
-            return
-        }
+    if (message == "/start") telegram.sendMenu(mainMenuBody)
 
     when (callbackData.lowercase()) {
         CALLBACK_LEARN_WORDS_CLICKED -> {
@@ -118,61 +97,41 @@ fun handleUpdate(
             telegram.answerCallbackQuery(callbackQueryId)
         }
 
-        CALLBACK_MENU_STATISTICS_CLICKED -> try {
+        CALLBACK_MENU_STATISTICS_CLICKED -> {
             telegram.sendMenu(
                 rawMenuBody = getBodyStatisticsMenu(chatId = chatId),
             )
             telegram.answerCallbackQuery(callbackQueryId)
-        } catch (e: Error) {
-            println("Ошибка при отправке меню статистики: ${e.message}")
-            return
         }
 
         CALLBACK_LOAD_WORDS_FILE_CLICKED -> {
-            try {
-                telegram.sendMenu(
-                    rawMenuBody = getBodyUploadWordsListMenu(chatId)
-                )
-                telegram.answerCallbackQuery(callbackQueryId)
-            } catch (e: Error) {
-                println("Ошибка при отправке меню загрузки файла: ${e.message}")
-                return
-            }
+            telegram.sendMenu(
+                rawMenuBody = getBodyUploadWordsListMenu(chatId)
+            )
+            telegram.answerCallbackQuery(callbackQueryId)
         }
 
-        CALLBACK_SHOW_STATISTICS_CLICKED -> try {
+        CALLBACK_SHOW_STATISTICS_CLICKED -> {
             telegram.sendMessage(
                 chatId = chatId,
                 text = getStatisticsString(trainer.getStatistics())
             )
             telegram.answerCallbackQuery(callbackQueryId)
-        } catch (e: Error) {
-            println("Ошибка при отправке статистики: ${e.message}")
-            return
         }
 
-        CALLBACK_RESET_STATISTICS_CLICKED -> try {
+        CALLBACK_RESET_STATISTICS_CLICKED -> {
             trainer.resetProgress()
             telegram.answerCallbackQuery(callbackQueryId, text = TEXT_COMPLETE_RESET_STATISTICS)
-        } catch (e: Error) {
-            println("Ошибка при сбросе статистики: ${e.message}")
-            return
         }
 
-        CALLBACK_EXIT_MAIN_MENU_CLICKED -> try {
+        CALLBACK_EXIT_MAIN_MENU_CLICKED -> {
             telegram.sendMenu(mainMenuBody)
             telegram.answerCallbackQuery(callbackQueryId)
-        } catch (e: Error) {
-            println("Ошибка при выходе в главное меню: ${e.message}")
-            return
         }
 
-        CALLBACK_GO_BACK_CLICKED -> try {
+        CALLBACK_GO_BACK_CLICKED -> {
             telegram.sendMenu(mainMenuBody)
             telegram.answerCallbackQuery(callbackQueryId)
-        } catch (e: Error) {
-            println("Ошибка при нажатии кнопки назад: ${e.message}")
-            return
         }
 
     }
@@ -181,39 +140,24 @@ fun handleUpdate(
         val answerIndex = callbackData.substringAfter(CALLBACK_ANSWER_PREFIX).toInt()
         val checkAnswerResult = trainer.checkAnswer(answerIndex)
         when (checkAnswerResult) {
-            true -> try {
-                telegram.sendMessage(
-                    chatId = chatId,
-                    text = TEXT_ANSWER_CORRECT
-                )
-            } catch (e: Error) {
-                println("Ошибка при отправке корректного результата ответа: ${e.message}")
-                return
-            }
+            true -> telegram.sendMessage(
+                chatId = chatId,
+                text = TEXT_ANSWER_CORRECT
+            )
 
-            false -> try {
-                telegram.sendMessage(
-                    chatId = chatId,
-                    text = "$TEXT_ANSWER_WRONG : ${currentQuestion?.correctWord?.original} - ${currentQuestion?.correctWord?.translate}"
-                )
-            } catch (e: Error) {
-                println("Ошибка при отправке результата неправильного ответа: ${e.message}")
-                return
-            }
+            false -> telegram.sendMessage(
+                chatId = chatId,
+                text = "$TEXT_ANSWER_WRONG : ${currentQuestion?.correctWord?.original} - ${currentQuestion?.correctWord?.translate}"
+            )
         }
-        try {
-            currentQuestion =
-                checkNextQuestionAndSend(
-                    trainer = trainer,
-                    chatId = chatId,
-                    botToken = botToken,
-                    json = json,
-                    callbackQueryId = callbackQueryId,
-                )
-        } catch (e: Error) {
-            println("Ошибра при отправке списка слов для изучения: ${e.message}")
-            return
-        }
+        currentQuestion =
+            checkNextQuestionAndSend(
+                trainer = trainer,
+                chatId = chatId,
+                botToken = botToken,
+                json = json,
+                callbackQueryId = callbackQueryId,
+            )
     }
 }
 
@@ -233,26 +177,18 @@ fun checkNextQuestionAndSend(
         json = json,
     )
     if (question == null)
-        try {
-            telegram.sendMessage(
-                chatId = chatId,
-                text = TEXT_ALL_WORDS_LEARNED
-            )
-        } catch (e: Error) {
-            println("Ошибка при отправке сообщения \"$TEXT_ALL_WORDS_LEARNED\" ${e.message}")
-        }
+        telegram.sendMessage(
+            chatId = chatId,
+            text = TEXT_ALL_WORDS_LEARNED
+        )
     else {
-        try {
-            telegram.sendMenu(
-                rawMenuBody = getBodyLearnWordsMenu(
-                    chatId = chatId,
-                    question = question,
-                )
+        telegram.sendMenu(
+            rawMenuBody = getBodyLearnWordsMenu(
+                chatId = chatId,
+                question = question,
             )
-            telegram.answerCallbackQuery(callbackQueryId)
-        } catch (e: Error) {
-            println("Ошибка при отправке меню изучения слов: ${e.message}")
-        }
+        )
+        telegram.answerCallbackQuery(callbackQueryId)
     }
     return question
 }
@@ -263,29 +199,21 @@ fun getUserWordsFileAndSave(chatId: Long, document: Document, telegram: Telegram
     val fileResponse = telegram.getFileInfo(
         getBodyRequestFileInfo(document.fileId)
     )
-    fileResponse.response.let { tgFile ->
+    fileResponse?.response.let { tgFile ->
         if (File(userCustomFileName).exists()) {
-            try {
-                telegram.sendMessage(
-                    chatId = chatId,
-                    text = TEXT_FILE_ALREADY_EXIST,
-                )
-            } catch (e: Error) {
-                println("Ошибка отправки сообщения о существующем файле ${e.message}")
-            }
+            telegram.sendMessage(
+                chatId = chatId,
+                text = TEXT_FILE_ALREADY_EXIST,
+            )
             return
         }
         val file =
             telegram.downloadFile(tgFile?.filePath)
         file?.copyTo(File(userCustomFileName).outputStream(), 16 * 1024)
-        try {
-            telegram.sendMessage(
-                chatId = chatId,
-                text = TEXT_FILE_LOADED_SUCCESSFUL,
-            )
-        } catch (e: Error) {
-            println("Ошибка отправки сообщения о успешной загрузке файла ${e.message}")
-        }
+        telegram.sendMessage(
+            chatId = chatId,
+            text = TEXT_FILE_LOADED_SUCCESSFUL,
+        )
     }
     File(userCustomFileName).readLines().forEach {
         File("$chatId$FILE_TEXT_EXT").appendText("\n$it")
